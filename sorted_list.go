388d8aaa334a878d51e3ff50b282a1aafebde58b
liuzhanliang <zhanliangliu@qq.com>
2018-05-31 15:18:51 +0800

    this->l

--- sorted_list.go
+++ sorted_list.go
@@ -15,39 +15,39 @@ func NewSortedList(compare CompareFunc) *SortedList {
 	return &SortedList{container: list.New(), compare: compare}
 }
 
-func (this *SortedList) Len() int64 {
-	return int64(this.container.Len())
+func (l *SortedList) Len() int64 {
+	return int64(l.container.Len())
 }
 
-func (this *SortedList) Front() *list.Element {
-	return this.container.Front()
+func (l *SortedList) Front() *list.Element {
+	return l.container.Front()
 }
 
-func (this *SortedList) Back() *list.Element {
-	return this.container.Back()
+func (l *SortedList) Back() *list.Element {
+	return l.container.Back()
 }
 
-func (this *SortedList) Clear() {
-	if this.Len() > 0 {
-		this.container = list.New()
+func (l *SortedList) Clear() {
+	if l.Len() > 0 {
+		l.container = list.New()
 	}
 }
 
 // Only Search from front to end, optimized for from end to front
-func (this *SortedList) Insert(item interface{}) {
-	element := this.container.Front()
+func (l *SortedList) Insert(item interface{}) {
+	element := l.container.Front()
 	for element != nil {
-		if this.compare(item, element.Value) <= 0 {
-			this.container.InsertBefore(item, element)
+		if l.compare(item, element.Value) <= 0 {
+			l.container.InsertBefore(item, element)
 			return
 		}
 		element = element.Next()
 	}
 	if element == nil {
-		this.container.PushBack(item)
+		l.container.PushBack(item)
 	}
 }
 
-func (this *SortedList) Remove(e *list.Element) interface{} {
-	return this.container.Remove(e)
+func (l *SortedList) Remove(e *list.Element) interface{} {
+	return l.container.Remove(e)
 }
